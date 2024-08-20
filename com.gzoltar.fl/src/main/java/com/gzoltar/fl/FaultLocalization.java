7788e0f4d6f394dc35ba137ab7b45d323fc72fd1
mguerrinha <mlguerrinha@gmail.com>
2024-08-20 11:40:28 +0100

    remove print

--- com.gzoltar.fl/src/main/java/com/gzoltar/fl/FaultLocalization.java
+++ com.gzoltar.fl/src/main/java/com/gzoltar/fl/FaultLocalization.java
@@ -65,7 +65,6 @@ public class FaultLocalization {
     ISpectrum filteredSpectrum = filter.filter(spectrum);
 
     this.fl.diagnose(filteredSpectrum);
-    System.out.println(agentConfigs.getImproveMultiplication());
     if (agentConfigs.getImproveMultiplication()) {
       ImproveMultiplication.improvement(filteredSpectrum);
     }
